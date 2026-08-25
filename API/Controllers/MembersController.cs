fb3afec95dcc2d050cb0647ba633d72442f6a21b
Hariharan Rajaram <Hariharan Rajaram@gmail.com>
2026-08-25 20:13:57 +0530

    Implemented Authentication and Authorizations.

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -2,12 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {   
-    [Route("api/[controller]")] //localhost:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -15,7 +14,8 @@ namespace API.Controllers
             var members = await context.Users.ToListAsync();
             return members;
         }
-
+        
+        [Authorize]
         [HttpGet("{id}")] //localhost:5001/api/members/hari-id
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
