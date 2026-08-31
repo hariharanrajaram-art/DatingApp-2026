0e1fbf702ecbae59508de0c4a2decddb4597d57f
Hariharan Rajaram <Hariharan Rajaram@gmail.com>
2026-08-31 14:36:26 +0530

    Added Seed Data, IMemberRepository and implamented IMemberRepository,  Updated Program.cs to register IMemberRepository and automatically create Db.

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -6,5 +6,24 @@ namespace API.Data
     public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<AppUser> Users { get; set; }
+
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>()
+                .HasOne(u => u.Member)
+                .WithOne(m => m.User)
+                .HasForeignKey<Member>(m => m.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Member>()
+                .HasMany(m => m.Photos)
+                .WithOne(p => p.Member)
+                .HasForeignKey(p => p.MemberId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
-}   
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
+using System.Reflection.Metadata;
 using API.Data;
 using API.Interfaces;
 using API.Middleware;
+using API.Repositories;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +21,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
@@ -45,4 +48,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+
+}
+catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+}
 app.Run();
